bb12330ffacc988b870ef69262519f11f1ee8940
Brown-Harry Boma <bomsy1@gmail.com>
2018-01-03 18:38:03 +0100

    Update2

---
+++ telegram/telegram/doctype/telebot_settings/telebot_settings.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Manqala Ltd. and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from frappe.utils import get_url
+import requests
+
+
+
+class TelebotSettings(Document):
+
+	def validate(self):
+		self.set_webhook()
+	
+
+	def set_webhook(self):
+		try:
+			url = "https://api.telegram.org/bot{}/setWebhook?".format(self.token)
+			url += "url={}/api/method/telegram.telegram_api.get_update".format(get_url())
+			r = requests.get(url,timeout=10)
+			frappe.logger().info(r.content)
+			frappe.logger().info(r.status_code)
+			# frappe.logger().info(url)
+		except Exception as e:
+			frappe.log_error(e,'Timeout for Telebot')
+
+
+
